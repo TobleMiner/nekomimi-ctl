2b55c9c5785ed1dd5733f89072a2ef057539583c
Tobias Schramm <tobleminer@gmail.com>
2019-08-01 01:39:42 +0200

    Utilize bh1750 service callbacks

--- main/bh1750_sensor.c
+++ main/bh1750_sensor.c
@@ -19,12 +19,27 @@ static void bh1750_sensor_free(struct sensor* sensor) {
   free(BH1750_SENSOR_FROM_SENSOR(sensor));
 }
 
-static esp_err_t bh1750_sensor_init(struct sensor* sensor, struct i2c_bus* bus, uint8_t i2c_addr) {
-  return bh1750_service_init(&BH1750_SENSOR_FROM_SENSOR(sensor)->bh, bus, i2c_addr);
-}
-
 static esp_err_t bh1750_sensor_get(struct sensor* sensor, sensor_param_t param, sensor_result_t* res, size_t len) {
   *res = bh1750_service_get_illuminance(&BH1750_SENSOR_FROM_SENSOR(sensor)->bh);
+  return ESP_OK;
+}
+
+static void bh1750_sensor_cb(void* priv) {
+  struct sensor* sensor = priv;
+  sensor_result_t res;
+  bh1750_sensor_get(sensor, SENSOR_PARAM_ILLUMINANCE, &res, sizeof(res));
+  sensors_report_result(sensor, SENSOR_PARAM_ILLUMINANCE, &res, sizeof(res));
+}
+
+static esp_err_t bh1750_sensor_init(struct sensor* sensor, struct i2c_bus* bus, uint8_t i2c_addr) {
+  esp_err_t err;
+  struct bh1750_service* bh = &BH1750_SENSOR_FROM_SENSOR(sensor)->bh;
+  err = bh1750_service_init(bh, bus, i2c_addr);
+  if(err) {
+    return err;
+  }
+
+  bh1750_service_set_cb(bh, bh1750_sensor_cb, sensor);
   return ESP_OK;
 }
 
