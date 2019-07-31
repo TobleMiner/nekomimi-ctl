2a2ae8088bed29e2d7cfac8f40f8214d17a54c24
Tobias Schramm <tobleminer@gmail.com>
2019-08-01 00:02:13 +0200

    Add container_of macro

--- main/util.h
+++ main/util.h
@@ -7,6 +7,10 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
+#define container_of(ptr, type, member) ({      \
+  const typeof(((type *)0)->member) * __mptr = (ptr); \
+  (type *)((char *)__mptr - offsetof(type, member)); })
+
 #define CLAMP(x, h, l) (max(min((x), (l)), (h)))
 
 #define ARRAY_LEN(arr) (sizeof((arr)) / sizeof(*(arr)))
